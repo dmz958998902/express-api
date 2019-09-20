2735dfa95d6cc0529f7cbf23f3ad8e6f1e0e37c4
Your Name <you@example.com>
2019-09-20 21:39:45 +0800

    跨域处理

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app=express();
 //处理req.body
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+//前后端分离跨域处理
+app.use((req,res,next)=>{
+    res.set("Access-Control-Allow-Origin","*");
+    next();
+});
 //静态资源托管
 app.use(express.static("public"));
 //引入各种路由中间件
